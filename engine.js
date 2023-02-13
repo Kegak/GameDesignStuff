5984aaefb9dfa73540f549f8e49a1bb8256ab124
Kegak <keeganjsims@gmail.com>
2023-02-13 14:24:31 -0600

    Made Github

---
+++ engine.js
@@ -0,0 +1,94 @@
+let canvas = document.querySelector("#canv")
+let ctx = canvas.getContext("2d");
+
+let keysDown = []
+let mouseX;
+let mouseY
+
+//Not the strings has to be all lowercase, e.g. keydown not keyDown or KeyDown
+document.addEventListener("keydown", keyDown)
+document.addEventListener("keyup", keyUp)
+
+document.addEventListener("mousedown", mouseDown);
+document.addEventListener("mouseup", mouseUp);
+document.addEventListener("mousemove", mouseMove);
+
+//0 is start scene, 1 main scene, 2 is dead scene
+let scene = 0;
+
+let pause = false
+
+function mouseDown(e){
+    //console.log("mouseDown: " + e.clientX + " " + e.clientY)
+  }
+function mouseUp(e){
+    //console.log("mouseUp: " + e.clientX + " " + e.clientY)
+  }
+function mouseMove(e){
+    //console.log("mouseMove: " + e.clientX + " " + e.clientY)
+  }
+  function keyUp(e) {
+    keysDown[e.key] = false
+    //console.log(e)
+    if (e.key == "ArrowLeft") {
+      console.log("Up Left")
+    }
+    if (e.key == "ArrowRight") {
+      console.log("Up Right")
+    }
+    if(e.key == "p"){
+        pause = !pause
+    }
+    
+  }
+
+  function keyDown(e) {
+    keysDown[e.key] = true
+    //console.log(e)
+    if (e.key == "ArrowLeft") {
+      console.log("Down Left")
+    }
+    if (e.key == "ArrowRight") {
+      console.log("Down Right")
+    }
+    //To prevent scrolling (if needed)
+    //This has to be in keyDown, not keyup
+    if(e.key == " "){
+      e.preventDefault()
+    }
+  }
+
+  function engineUpdate(){
+    if(pause) return
+    update()
+  }
+  function engineDraw(){
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    draw()
+  }
+
+  function start(title){
+    document.title = title
+    function gameLoop() {
+        engineUpdate()
+        engineDraw()
+      }
+  
+      setInterval(gameLoop, 1000 / 25)
+  }
+
+function Q_InvSqrt(num){
+  //got from here: https://medium.com/@adrien.za/fast-inverse-square-root-in-go-and-javascript-for-fun-6b891e74e5a8
+  //inspration though: https://en.wikipedia.org/wiki/Fast_inverse_square_root
+  buffer = new ArrayBuffer(4)
+  view = new DataView(buffer)
+  var y, x2 = num * 0.5, threehalfs = 1.5
+  view.setFloat32(0, num) //evil floating point bit level hacking
+  view.setUint32(0, 0x5F375A86 - (view.getUint32(0) >> 1)) //what the duck?
+  y = view.getFloat32(0)
+  y *= (threehalfs - (x2 * y * y)) //1st iteration
+  //y *= (threehalfs - (x2 * y * y)) //2nd iteration, this can be removed
+  return y
+  
+}
---
+++ pong.js
@@ -0,0 +1,120 @@
+let margin = 20;
+let size = 100;
+
+
+
+let pongX = margin + size / 2
+let pongY = margin + size / 2
+let pongVX = 3
+let pongVY = 2
+let paddleX = margin + size / 2
+let paddleWidth = 40;
+let points = 0;
+
+
+
+
+
+function update() {
+
+  if (scene == 0) {
+    if(keysDown["a"]){
+      scene = 1;
+    }
+  }
+  else if (scene == 1) {
+
+    //Model of MVC
+    pongX += pongVX
+    pongY += pongVY
+
+    if (pongX > margin + size) {
+      pongVX *= -1
+    }
+    if (pongY > margin + size) {
+      //Check for a collision with the paddle
+      if (paddleX - paddleWidth / 2 <= pongX && paddleX + paddleWidth / 2 >= pongX){
+        pongVY *= -1
+        points++}
+      else {
+        console.log("You are dead")
+        scene = 2;
+      }
+    }
+    if (pongX < margin) {
+      pongVX *= -1
+    }
+    if (pongY < margin) {
+      pongVY *= -1
+    }
+
+    //Update the paddle based on input
+    if (keysDown["ArrowLeft"]) {
+      paddleX -= 2;
+    }
+    else if (keysDown["ArrowRight"]) {
+      paddleX += 2
+    }
+
+    //Constrain the paddle position
+    if (paddleX < margin + paddleWidth / 2) {
+      paddleX = paddleWidth / 2 + margin
+    }
+    if (paddleX > margin - paddleWidth / 2 + size) {
+      paddleX = -paddleWidth / 2 + margin + size
+    }
+  }
+  else {
+    //Scene 2
+    if(keysDown["a"]){
+      scene = 0;
+    }
+  }
+}
+
+function draw() {
+
+
+  if (scene == 0) {
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "white";
+    ctx.fillText("Welcome to Pong", 100, 100);
+  }
+  else if (scene == 1) {
+    //View part of MVC
+    ctx.fillStyle = "green"
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    ctx.strokeStyle = "black"
+    ctx.beginPath()
+    ctx.moveTo(margin, margin)
+    ctx.lineTo(margin + size, margin)
+    ctx.lineTo(margin + size, margin + size)
+    ctx.moveTo(margin, margin + size)
+    ctx.lineTo(margin, margin)
+    ctx.stroke()
+
+    //Now draw the paddle
+    ctx.beginPath()
+    ctx.moveTo(paddleX - paddleWidth / 2, margin + size)
+    ctx.lineTo(paddleX + paddleWidth / 2, margin + size)
+    ctx.stroke()
+
+    ctx.fillStyle = "blue"
+
+    ctx.beginPath()
+    ctx.arc(pongX, pongY, 5, 0, Math.PI * 2)
+    ctx.fill()
+
+    ctx.fillStyle = "white"
+    ctx.fillText(points, 0, 10);
+  }
+  else{
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "red";
+    ctx.fillText("You died", 100, 100);
+  }
+
+}
